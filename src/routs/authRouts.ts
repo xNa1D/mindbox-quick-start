cd3c702dd41b9c2dd5a59c1e73c4645aa1270f04
Petr <nikitin.p.94@gmail.com>
2020-11-17 22:10:35 +0300

    add rout for handle login
    for login it will create instance of user with email and password,
    send request to mindbox
    and in case of success create new jwt

--- src/routs/authRouts.ts
+++ src/routs/authRouts.ts
@@ -1,3 +1,39 @@
-const authRouts = () => {};
+import { Request, Response, NextFunction, Router } from "express";
+
+import User from "../models/User";
+import generateAccessToken from "../helpers/generateAccessToken";
+declare namespace myExpress {
+  interface Request {
+    body: {
+      email: string;
+      password: string;
+    };
+  }
+
+  interface Response {
+    body: {
+      isLoggedIn: boolean;
+      token: string;
+    };
+  }
+}
+
+const authRouts = () => {
+  const router = Router();
+
+  router.post("/auth", async (req: Request, res: Response) => {
+    const user = new User(req.body.email, req.body.password);
+    const isLoggedSuccessfully = await user.loginStuff();
+
+    let accessToken: string;
+
+    if (isLoggedSuccessfully) {
+      accessToken = generateAccessToken(user.email);
+      res.send(accessToken);
+    } else {
+      res.sendStatus(403);
+    }
+  });
+};
 
 export default authRouts;
