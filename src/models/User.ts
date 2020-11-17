b2b80cdc9f94dbfe2b9c73c8b52108722b110b67
Petr <nikitin.p.94@gmail.com>
2020-11-17 22:08:47 +0300

    add User class
    it can makes new password,
    ckeck existing user in Mindbox
    registrate new user
    and login
    
    for all request used axios

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,104 @@
+import axios from "axios";
+import generator from "generate-password";
+
+type Status =
+  | "Success"
+  | "ValidationError"
+  | "ProtocolError"
+  | "InternalServerError";
+
+type FindProcessingStatus = "NotFound" | "Found";
+type LoginProcessingStatus = "AuthenticationSucceeded" | "AuthenticationFailed";
+
+type FindStuffAnswer = {
+  status: Status;
+  customer: {
+    processingStatus: FindProcessingStatus;
+  };
+};
+type LoginStufffAnswer = {
+  status: Status;
+  customer: {
+    processingStatus: LoginProcessingStatus;
+  };
+};
+
 class User {
-  constructor() {}
+  email: string;
+  private password: string;
+
+  constructor(email: string, password?: string) {
+    this.email = email;
+    this.password = password;
+  }
+
+  makeNewPassword() {
+    this.password = generator.generate({
+      length: 10,
+      numbers: true,
+    });
+    return this.password;
+  }
+
+  async isStuffExistInMindbox(): Promise<boolean> {
+    const user = await axios.post<FindStuffAnswer>(
+      `https://api.mindbox.ru/v3/operations/sync?endpointId=${process.env.ENDPOINT}&operation=QuickStart.GetStaff`,
+      {
+        customer: {
+          email: this.email,
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Accept: "application/json",
+          Authorization: `Mindbox secretKey="${process.env.SECRET_KEY}"`,
+        },
+      }
+    );
+
+    return user.data.customer.processingStatus === "Found";
+  }
+
+  async registrStuff(): Promise<{}> {
+    return await axios.post(
+      `https://api.mindbox.ru/v3/operations/sync?endpointId=${process.env.ENDPOINT}&operation=QuickStart.Reg`,
+      {
+        customer: {
+          email: this.email,
+          password: this.makeNewPassword(),
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Accept: "application/json",
+          Authorization: `Mindbox secretKey="${process.env.SECRET_KEY}"`,
+        },
+      }
+    );
+  }
+
+  async loginStuff(): Promise<boolean> {
+    const user = await axios.post<LoginStufffAnswer>(
+      `https://api.mindbox.ru/v3/operations/sync?endpointId=${process.env.ENDPOINT}&operation=QuickStart.Auth`,
+      {
+        customer: {
+          email: this.email,
+          password: this.password,
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Accept: "application/json",
+          Authorization: `Mindbox secretKey="${process.env.SECRET_KEY}"`,
+        },
+      }
+    );
+
+    return user.data.customer.processingStatus === "AuthenticationSucceeded";
+  }
 }
 
 export default User;
