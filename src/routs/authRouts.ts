46eadbe630ca5819262e39373acddc049575fc63
Petr <nikitin.p.94@gmail.com>
2020-11-18 11:54:05 +0300

    fix router for auth

--- src/routs/authRouts.ts
+++ src/routs/authRouts.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction, Router } from "express";
 
 import User from "../models/User";
 import generateAccessToken from "../helpers/generateAccessToken";
+
 declare namespace myExpress {
   interface Request {
     body: {
@@ -18,22 +19,24 @@ declare namespace myExpress {
   }
 }
 
-const authRouts = () => {
-  const router = Router();
+const authRouter = Router();
+authRouter.post("/auth", async (req: Request, res: Response) => {
+  const user = new User(req.body.email, req.body.password);
+  const isLoggedSuccessfully = await user.loginStuff();
 
-  router.post("/auth", async (req: Request, res: Response) => {
-    const user = new User(req.body.email, req.body.password);
-    const isLoggedSuccessfully = await user.loginStuff();
-
-    let accessToken: string;
-
+  let accessToken: string;
+  try {
     if (isLoggedSuccessfully) {
       accessToken = generateAccessToken(user.email);
       res.send(accessToken);
     } else {
       res.sendStatus(403);
     }
-  });
-};
+  } catch (error) {
+    console.log(error);
 
-export default authRouts;
+    res.sendStatus(503);
+  }
+});
+
+export default authRouter;
