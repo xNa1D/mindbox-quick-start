900c59a2115733e8d90848808c7ca48306d98f8e
Petr <nikitin.p.94@gmail.com>
2020-11-21 15:53:38 +0300

    add routes and middleweares

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import * as bodyParser from "body-parser";
-
+import coockieParser from "cookie-parser";
 import * as dotenv from "dotenv";
+import morgan from "morgan";
 
-import authRouts from "./routs/authRouts";
+import userRoutes from "./routs/userRoutes";
+import scenariosRoutes from "./routs/scenariosRoutes";
+import pagesRoutes from "./routs/pagesRoutes";
 
 const app = express();
 
 dotenv.config();
 
 app.use(bodyParser.json());
-app.use(express.static("static"));
+app.use(coockieParser());
+app.use(express.static("./src/static"));
 
-app.use("/api/user", authRouts);
+app.use("/", pagesRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/scenario", scenariosRoutes);
 
 const port = process.env.PORT || 3000;
 
