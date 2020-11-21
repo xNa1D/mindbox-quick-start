395d8e8da23dfe1419c0e0c0b94e11450aa77cd4
Petr <nikitin.p.94@gmail.com>
2020-11-21 15:56:02 +0300

    refactor scenario methods
    поправил вызов функций гост инспектора,
    чтобы соответсоввать документации

--- src/models/Scenarios.ts
+++ src/models/Scenarios.ts
@@ -1,34 +1,48 @@
 import axios from "axios";
 
-export const createBasicEcommersOperations = async (projectName: string) =>
-  axios.post(
-    `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
-    { projectName }
-  );
+import { Scenarios } from "../index.d";
 
-export const createLoayltyOnlineOperations = async (
-  projectName: string,
-  campaignNumber: number
-) =>
-  axios.post(
-    `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
-    { projectName, campaign: campaignNumber }
-  );
+const scenarios: Scenarios = {
+  ecommerce: async (projectName: string, campaignNumber?: number) =>
+    axios.post(
+      `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+      { projectName },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ),
+  loyaltyOnline: async (projectName: string, campaignNumber: number) =>
+    axios.post(
+      `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+      { projectName, campaign: campaignNumber },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ),
+  loyaltyOfline: async (projectName: string, campaignNumber: number) =>
+    axios.post(
+      `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+      { projectName, campaign: campaignNumber },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ),
+  mobilePush: async (projectName: string, campaignNumber: number) =>
+    axios.post(
+      `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+      { projectName, campaign: campaignNumber },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ),
+};
 
-export const createLoayltyOfflineOperations = async (
-  projectName: string,
-  campaignNumber: number
-) =>
-  axios.post(
-    `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
-    { projectName, campaign: campaignNumber }
-  );
-
-export const createMobilePushOperations = async (
-  projectName: string,
-  campaignNumber: number
-) =>
-  axios.post(
-    `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
-    { projectName, campaign: campaignNumber }
-  );
+export default scenarios;
