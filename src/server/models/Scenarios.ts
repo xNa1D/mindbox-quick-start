19925612062f546b02c9a60205d78bfc80c9b5ed
Petr <nikitin.p.94@gmail.com>
2020-11-26 23:04:32 +0300

    made correct scenario urls

--- src/server/models/Scenarios.ts
+++ src/server/models/Scenarios.ts
@@ -15,7 +15,7 @@ const scenarios: Scenarios = {
     ),
   loyaltyOnline: async (projectName: string, campaignNumber: number) =>
     axios.post(
-      `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+      `https://api.ghostinspector.com/v1/tests/5ed5315fe1d6aa3e73eeac22/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
       { projectName, campaign: campaignNumber },
       {
         headers: {
@@ -23,19 +23,30 @@ const scenarios: Scenarios = {
         },
       }
     ),
-  loyaltyOfline: async (projectName: string, campaignNumber: number) =>
-    axios.post(
-      `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+  loyaltyOfline: async (projectName: string, campaignNumber: number) => {
+    await axios.post(
+      `https://api.ghostinspector.com/v1/tests/5ecbae5297e4531b3aaaf62e/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
       { projectName, campaign: campaignNumber },
       {
         headers: {
           "Content-Type": "application/json",
         },
       }
-    ),
+    );
+    return axios.post(
+      `https://api.ghostinspector.com/v1/tests/5f113490d5acb022c96d5bf5/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+      { projectName, campaign: campaignNumber },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  },
   mobilePush: async (projectName: string, campaignNumber: number) =>
     axios.post(
-      `https://api.ghostinspector.com/v1/tests/5ec6c26197e4531b3a9d9864/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+      `https://api.ghostinspector.com/v1/tests/5fb2689f89be016e97029052/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77
+      `,
       { projectName, campaign: campaignNumber },
       {
         headers: {
