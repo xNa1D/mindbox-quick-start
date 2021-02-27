3e1a149e5fabdaaccd9aaf9e560094a446643bae
Petr Nikitin <nikitin.p.94@gmail.com>
2021-02-27 16:44:54 +0300

    add react to project

---
+++ src/client/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import  './styles/style.css';
+
+import App from "./script/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
