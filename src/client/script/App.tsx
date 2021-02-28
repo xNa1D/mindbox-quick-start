648c34bf14b11969dfa28d8edbd28bd3abc11263
Petr Nikitin <nikitin.p.94@gmail.com>
2021-02-28 21:23:32 +0300

    add AuthProvider to app

--- src/client/script/App.tsx
+++ src/client/script/App.tsx
@@ -4,40 +4,46 @@ import Login from './pages/Login'
 import Registration from './pages/Registration';
 import Scenario from "./pages/Scenario";
 
+import { ProvideAuth } from 'client/script/auth/useAuth';
+
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <Router>
-      <section className="header">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/registration">registration</Link>
-            </li>
-            <li>
-              <Link to="/scenario">scenario</Link>
-            </li>
-          </ul>
-        </nav>
-      </section>
-      <main className="main">
-        <Logo />
-        <Switch>
-          <Route path="/registration">
-            <Registration />
-          </Route>
-          <Route path="/scenario">
-            <Scenario />
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </main>
-   </Router>
+    <ProvideAuth>
+      <Router>
+        <section className="header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/registration">registration</Link>
+              </li>
+              <li>
+                <Link to="/scenario">scenario</Link>
+              </li>
+            </ul>
+          </nav>
+        </section>
+        <main className="main">
+          <Logo />
+          <Switch>
+            <Route path="/registration">
+              <Registration />
+            </Route>
+            <Route path="/scenario">
+              <Scenario />
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </ProvideAuth>
   );
 }
+
+
