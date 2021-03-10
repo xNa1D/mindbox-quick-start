9369f51ca882cd060f3dbf765fc55a7e2f128ec7
Petr <nikitin.p.94@gmail.com>
2021-03-10 21:01:45 +0300

    add create user ruquest

--- src/client/script/api/userRequests.ts
+++ src/client/script/api/userRequests.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { AuthRequestBody } from "src/declarations";
+import { AuthRequestBody, RegistrationRequest } from "src/declarations";
 
 export const loginUser = (user: AuthRequestBody) => {
   return axios.post("/api/user/auth", user, {
@@ -9,6 +9,11 @@ export const loginUser = (user: AuthRequestBody) => {
 };
 export const checkToken = (token: string) => {
   return axios.post("/api/user/auth", token, {
+    headers: { "content-type": "application/json" },
+  });
+};
+export const createUser = (user: RegistrationRequest) => {
+  return axios.post("/api/user/reg", user, {
     headers: { "content-type": "application/json" },
   });
 };
