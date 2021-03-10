beb73effd78fc500397b34376e66606c723eb4ee
Petr <nikitin.p.94@gmail.com>
2021-03-10 22:00:45 +0300

    add mocks

---
+++ src/__mocks__/userRequests.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+import { AuthRequestBody, RegistrationRequest } from "src/declarations";
+
+export const loginUser = jest.fn();
+export const checkToken = jest.fn();;
+export const createUser = jest.fn();;
+
