ba79699a7bb77dfece96457ceb08e4dd7420f4c5
Petr <nikitin.p.94@gmail.com>
2021-03-11 22:28:12 +0300

    add privete route component

---
+++ src/client/script/helpers/PrivateRoute.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  Route, 
+  Redirect, 
+} from "react-router-dom";
+
+import useAuth from 'client/script/hooks/useAuth';
+
+
+function PrivateRoute({ children, ...rest }:any) {
+  let auth = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth.isLoggedIn ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+export default PrivateRoute
