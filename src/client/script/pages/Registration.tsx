56d91cec857ab569c2466788148d74c8dacbc2fa
Petr <nikitin.p.94@gmail.com>
2021-03-12 20:55:04 +0300

    add registration and add tests

--- src/client/script/pages/Registration.tsx
+++ src/client/script/pages/Registration.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+
+import { createUser } from "client/script/api/userRequests";
+import { handleEmailInput } from "client/script/helpers/inputChanges";
 
 const Registration = () => {
+  const [user, setUser] = useState({ email: "" });
+  const [formError, setFormError] = useState("");
+  const [isCreationSuccess, setIsCreationSuccess] = useState(false);
+  const [tick, setTick] = useState(5);
+
+  const tickTimer = () => {
+    setInterval(() => setTick(tick - 1), 100);
+  };
+
+  
+  const handleFormSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    
+    try {
+      await createUser(user);
+      setIsCreationSuccess(true);
+      tickTimer();
+      setTimeout(() => <Redirect to="/" />, 500);
+    } catch (error) {
+      setFormError(error.data);
+    }
+  };
+
   return (
-    <form className="ui form" id="auth__form_registration">
+    <form
+      className="ui form"
+      id="auth__form_registration"
+      onSubmit={handleFormSubmit}
+    >
       <fieldset className="field form__input-container">
         <div className="fluid field">
           <label htmlFor="email" className="form__label">
@@ -15,6 +46,10 @@ const Registration = () => {
               id="email"
               name="email"
               required
+              value={user.email}
+              onChange={(event) => {
+                setUser({ email: handleEmailInput(event.target.value) });
+              }}
             />
             <div className="ui basic label">@mindbox.ru</div>
           </div>
@@ -31,27 +66,31 @@ const Registration = () => {
           Регистрация
         </button>
       </div>
-      <div className="ui error message " id="error">
-        <div className="header">Такого пользователя нет</div>
-        <p>
-          Возможно ошибка в почте. <br /> Если ошибки нет, то напиши в слаке
-          @moskalev
-        </p>
-      </div>
-
-      <div className="ui success message" id="success">
-        <div className="header">Зарегистрирован!</div>
-        <p>
-          Пароль мы отправили на почту.{" "}
-          <a href="https://mail.google.com/" target="_blank">
-            Проверить в gmail
-          </a>
-        </p>
-        <p>
-          Через <span id="timer">5</span>с откроется страница логина. <br />{" "}
-          <a href="/">Но можно перейти прямо сейчас</a>{" "}
-        </p>
-      </div>
+      {formError && (
+        <div className="ui error message " id="error">
+          <div className="header">Ошибка регистрации</div>
+          <p>{formError}</p>
+          <p>
+            Возможно ошибка в почте. <br /> Если ошибки нет, то напиши в слаке
+            @moskalev
+          </p>
+        </div>
+      )}
+      {isCreationSuccess && (
+        <div className="ui success message" id="success">
+          <div className="header">Зарегистрирован!</div>
+          <p>
+            Пароль мы отправили на почту.{" "}
+            <a href="https://mail.google.com/" target="_blank">
+              Проверить в gmail
+            </a>
+          </p>
+          <p>
+            Через <span id="timer">{tick}</span>с откроется страница логина.{" "}
+            <br /> <a href="/">Но можно перейти прямо сейчас</a>{" "}
+          </p>
+        </div>
+      )}
     </form>
   );
 };
