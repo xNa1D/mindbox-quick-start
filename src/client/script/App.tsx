5d63649511d8f3407ac5f96046b955a3355e5b95
Petr Nikitin <nikitin.p.94@gmail.com>
2021-03-14 22:16:35 +0300

    add coockie provider

--- src/client/script/App.tsx
+++ src/client/script/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {CookiesProvider} from 'react-cookie'
 
 import PrivateRoute from 'client/script/helpers/PrivateRoute'
 import Logo from "./components/Logo";
@@ -11,39 +12,41 @@ import { ProvideAuth } from 'src/client/script/hooks/useAuth';
 
 export default function App() {
   return (
-    <ProvideAuth>
-      <Router>
-        <section className="header">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/registration">registration</Link>
-              </li>
-              <li>
-                <Link to="/scenario">scenario</Link>
-              </li>
-            </ul>
-          </nav>
-        </section>
-        <main className="main">
-          <Logo />
-          <Switch>
-            <Route path="/registration">
-              <Registration />
-            </Route>
-            <PrivateRoute path="/scenario">
-              <Scenario />
-            </PrivateRoute>
-            <Route path="/">
-              <Login />
-            </Route>
-          </Switch>
-        </main>
-      </Router>
-    </ProvideAuth>
+    <CookiesProvider>
+      <ProvideAuth>
+        <Router>
+          <section className="header">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/registration">registration</Link>
+                </li>
+                <li>
+                  <Link to="/scenario">scenario</Link>
+                </li>
+              </ul>
+            </nav>
+          </section>
+          <main className="main">
+            <Logo />
+            <Switch>
+              <Route path="/registration">
+                <Registration />
+              </Route>
+              <PrivateRoute path="/scenario">
+                <Scenario />
+              </PrivateRoute>
+              <Route path="/">
+                <Login />
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      </ProvideAuth>
+    </CookiesProvider>
   );
 }
 
