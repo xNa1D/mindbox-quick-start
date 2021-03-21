923dfa1c4fdb512e61c7e9b40bc5a0a6060a9fbf
Petr <nikitin.p.94@gmail.com>
2021-03-21 09:24:08 +0300

    update mocks

--- src/__tests__/client/hooks/useAuth.tsx
+++ src/__tests__/client/hooks/useAuth.tsx
@@ -1,10 +1,11 @@
-import React, { ReactComponentElement, ReactElement } from "react";
+import React from "react";
 import {
   render,
   screen,
   fireEvent,
   act,
   waitFor,
+  prettyDOM,
 } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import axios from "axios";
@@ -14,14 +15,36 @@ import useAuth, { ProvideAuth } from "client/script/hooks/useAuth";
 import MockConsumer from "../../../__mocks__/MockConsumer";
 
 import { loginUser, checkToken } from "client/script/api/userRequests";
-// import { User, AuthUserResponse } from "src/declarations";
 
 jest.mock("axios");
 jest.mock("client/script/api/userRequests");
 
+beforeEach(() => {
+  (loginUser as jest.Mock).mockResolvedValue({
+    response: { status: 200, data: "ok" },
+  });
+  (checkToken as jest.Mock).mockResolvedValue({
+    response: { status: 200, data: "ok" },
+  });
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+jest.mock("universal-cookie", () => {
+  const mCookie = {
+    set: jest.fn(),
+    get: jest.fn(),
+  };
+  return jest.fn(() => mCookie);
+});
+
 const customRender = (ui: any, { providerProps, ...renderOptions }: any) => {
   return render(
-    <ProvideAuth {...providerProps}>{ui}</ProvideAuth>,
+    <MemoryRouter>
+      <ProvideAuth {...providerProps}>{ui}</ProvideAuth>
+    </MemoryRouter>,
     renderOptions
   );
 };
@@ -57,16 +80,14 @@ describe("MockConsumer api calls", () => {
   });
 
   test(" should render login errors on rejection", async (done) => {
-    const mockedLogin = loginUser as jest.Mock;
-    mockedLogin.mockRejectedValue({ status: 401, data: "Login Error" });
-
-    customRender(<MockConsumer />, {});
-
-    const submitBtn = screen.getByText("Login");
-    await act(async () => {
-      fireEvent.click(submitBtn);
+    (loginUser as jest.Mock).mockRejectedValue({
+      response: { status: 401, data: "Login Error" },
     });
 
+    customRender(<MockConsumer />, {});
+    await act(async () => {
+      fireEvent.click(screen.getByText("Login"));
+    });
     expect(screen.getByText(/^Errors:/).textContent).toBe(
       "Errors: Login Error"
     );
@@ -74,8 +95,9 @@ describe("MockConsumer api calls", () => {
   });
 
   test(" should set login state to false on rejection", async (done) => {
-    const mockedLogin = loginUser as jest.Mock;
-    mockedLogin.mockRejectedValue({ status: 401, data: "Login Error" });
+    (loginUser as jest.Mock).mockRejectedValue({
+      response: { status: 401, data: "Login Error" },
+    });
 
     customRender(<MockConsumer />, {});
 
@@ -89,8 +111,9 @@ describe("MockConsumer api calls", () => {
   });
 
   test(" should change login status on resolving", async (done) => {
-    const mockedLogin = loginUser as jest.Mock;
-    mockedLogin.mockResolvedValue({status: 200, data: "Login Success"});
+    (loginUser as jest.Mock).mockResolvedValue({
+      response: { status: 200, data: "Login Success" },
+    });
 
     customRender(<MockConsumer />, {});
 
