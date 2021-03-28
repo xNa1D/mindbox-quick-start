135827b6884fee8708632291e559da116b8f4ab4
Petr Nikitin <nikitin.p.94@gmail.com>
2021-03-28 13:53:16 +0300

    change css file of semantic-ui

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import  './styles/vendor/semantic.css';
+import "semantic-ui-css/semantic.min.css";
 
 import App from "./script/App";
 
