cff0b53de9d06d7a87a54646dc5d4fb370c20d89
Petr Nikitin <nikitin.p.94@gmail.com>
2021-03-28 13:04:38 +0300

    made scanrio call common

--- src/server/models/Scenarios.ts
+++ src/server/models/Scenarios.ts
@@ -1,60 +1,19 @@
 import axios from "axios";
+import { StartScenarioBody } from "src/declarations";
 
-import { ScenarioApiCalls } from "src/declarations";
-import config from "../../config";
+const startScenario = async (
+  scenarioApiAddress: string,
+  projectName: string,
+  campaign: number
+) =>
+  await axios.post(
+    scenarioApiAddress,
+    { projectName, campaign },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-const scenarios: ScenarioApiCalls = {
-  ecommerce: async (projectName: string, campaignNumber?: number) =>
-    axios.post(
-      config.scenarioApi.ecommerce,
-      { projectName },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    ),
-  loyaltyOnline: async (projectName: string, campaignNumber: number) =>
-    axios.post(
-      config.scenarioApi.loyaltyOnline,
-      { projectName, campaign: campaignNumber },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    ),
-  loyaltyOffline: async (projectName: string, campaignNumber: number) => {
-    await axios.post(
-      config.scenarioApi.loyaltyOffline,
-      { projectName, campaign: campaignNumber },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    return axios.post(
-      config.scenarioApi.mobilePush,
-      { projectName, campaign: campaignNumber },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-  },
-  mobilePush: async (projectName: string, campaignNumber: number) =>
-    axios.post(
-      `https://api.ghostinspector.com/v1/tests/5fb2689f89be016e97029052/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77
-      `,
-      { projectName, campaign: campaignNumber },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    ),
-};
-
-export default scenarios;
+export default startScenario;
