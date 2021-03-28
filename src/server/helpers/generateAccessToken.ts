578e119077e57fb77cbe6d9157bea17b7fbcb6a6
Petr Nikitin <nikitin.p.94@gmail.com>
2021-03-28 23:19:08 +0300

    add fields into token for save data from admin panel

--- src/server/helpers/generateAccessToken.ts
+++ src/server/helpers/generateAccessToken.ts
@@ -1,9 +1,17 @@
 import * as jwt from "jsonwebtoken";
 
-const generateAccessToken = (email: string): string => {
-  return jwt.sign({ email }, process.env.JWT_SECRET || "test_secret", {
-    expiresIn: "1d",
-  });
+const generateAccessToken = (
+  email: string,
+  project?: string,
+  tokenFromAdminPanel?: string
+): string => {
+  return jwt.sign(
+    { email, project, tokenFromAdminPanel },
+    process.env.JWT_SECRET || "test_secret",
+    {
+      expiresIn: "1d",
+    }
+  );
 };
 
 export default generateAccessToken;
