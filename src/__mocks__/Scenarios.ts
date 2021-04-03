cc36cd26933781a151f2abb6d93f116576d9e533
Petr Nikitin <nikitin.p.94@gmail.com>
2021-04-03 22:29:39 +0300

    fix mocks

--- src/__mocks__/Scenarios.ts
+++ src/__mocks__/Scenarios.ts
@@ -1,22 +1,5 @@
-import { ScenarioApiCalls } from "src/declarations"
+import { ScenarioApiCalls } from "src/declarations";
 
-const scenarios: ScenarioApiCalls = {
-  ecommerce: jest.fn().mockImplementation(async (projectName: string) => ""),
-  loyaltyOnline: jest
-    .fn()
-    .mockImplementation(async (projectName: string, campaignNumber: number) =>
-      jest.fn().mockResolvedValue("")
-    ),
-  loyaltyOffline: jest
-    .fn()
-    .mockImplementation(async (projectName: string, campaignNumber: number) =>
-      jest.fn().mockResolvedValue("")
-    ),
-  mobilePush: jest
-    .fn()
-    .mockImplementation(async (projectName: string, campaignNumber: number) =>
-      jest.fn().mockResolvedValue("")
-    ),
-};
+const scenarios = jest.fn();
 
 export default scenarios;
