8bf94a211d1a2044833986ab9dd9a7cee35db9ad
Petr Nikitin <nikitin.p.94@gmail.com>
2021-04-03 15:00:06 +0300

    Add tests

--- src/__tests__/Scenarios.test.ts
+++ src/__tests__/Scenarios.test.ts
@@ -11,6 +11,12 @@ describe("startScenario", () => {
   it("should return 200 if resolve", async () => {
     axios.post = jest.fn().mockResolvedValue({
       status: 200,
+      data: {
+        code: "SUCCESS",
+        data: {
+          passing: true
+        }
+      },
     });
 
     const res = await startScenario(
@@ -18,10 +24,10 @@ describe("startScenario", () => {
       mockBody.projectName,
       mockBody.campaign
     );
-    expect(res.status).toBe(200);
+    expect(res).toBe(true);
   });
 
-  it("should trrow on network error", async () => {
+  it("should throw on network error", async () => {
     axios.post = jest.fn().mockRejectedValue({
       status: 500,
     });
@@ -34,6 +40,53 @@ describe("startScenario", () => {
       );
     } catch (error) {
       expect(error).toBeTruthy();
+    }
+  });
+
+  it("should throw internal error of code not SUCCESS", async () => {
+    axios.post = jest.fn().mockResolvedValue({
+      status: 200,
+      data: {
+        code: "ERROR",
+        data: {
+          passing: true,
+        },
+      },
+    });
+    try {
+      await await startScenario(
+        mockBody.scenarioApiAddress,
+        mockBody.projectName,
+        mockBody.campaign
+      );
+    } catch (error) {
+      expect(error.toString()).toBe("Error: Internal error in Scenario Server");
+    }
+  });
+
+  it("should throw internal error of code not SUCCESS", async () => {
+    axios.post = jest.fn().mockResolvedValue({
+      status: 200,
+      data: {
+        code: "SUCCESS",
+        data: {
+          passing: false,
+          error: {
+            details: "Test run reached 10 minute time limit and was stopped",
+          },
+        },
+      },
+    });
+    try {
+      await await startScenario(
+        mockBody.scenarioApiAddress,
+        mockBody.projectName,
+        mockBody.campaign
+      );
+    } catch (error) {
+      expect(error.toString()).toBe(
+        "Error: Test run reached 10 minute time limit and was stopped"
+      );
     }
   });
 });
