410dda1abb35ea35dfa4fb11a68efe92c24dad45
Petr Nikitin <nikitin.p.94@gmail.com>
2021-04-03 22:29:23 +0300

    refactor saving results

--- src/server/models/Scenarios.ts
+++ src/server/models/Scenarios.ts
@@ -1,28 +1,45 @@
 import axios from "axios";
 import { ScenarioResult } from "src/ScenarioResult";
+import { StepsEntity } from "src/ScenarioResult";
 
 const startScenario = async (
-  scenarioApiAddress: string,
+  scenarioApiAddress: string[],
   projectName: string,
   campaign: number
 ) => {
-  const result = await axios.post<ScenarioResult>(
-    scenarioApiAddress,
-    { projectName, campaign },
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  let resultStatus = {
+    status: "",
+    errorMessage: "",
+  };
+  let resultSteps: StepsEntity[] = [];
 
-  if (result.data.code !== "SUCCESS") {
-    throw new Error("Internal error in Scenario Server");
-  } else if (!result.data.data.passing) {
-     throw new Error(result.data.data.error?.details);
-  } else {
-    return true;
+  for (const api of scenarioApiAddress) {
+    const result = await axios.post<ScenarioResult>(
+      `https://api.ghostinspector.com/v1/tests/${api}/execute/?apiKey=777edc3b47a553359340c186dca0a1923bc51c77`,
+      { projectName, campaign },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    resultSteps = [...resultSteps, ...(result.data.data.steps || [])];
+
+    if (result.data.code !== "SUCCESS") {
+      resultStatus.status = "ERROR";
+      resultStatus.errorMessage = "Internal error in Scenario Server";
+    } else if (!result.data.data.passing) {
+      resultStatus.status = "ERROR";
+      resultStatus.errorMessage = result.data.data.error?.details || "";
+    } else {
+      resultStatus.status = "SUCCESS";
+    }
   }
+  return {
+    resultStatus,
+    resultSteps,
+  };
 };
 
 export default startScenario;
