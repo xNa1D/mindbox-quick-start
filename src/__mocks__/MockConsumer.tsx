dfb325188c32fa3a53e8761aac53b91dcfb8c59a
Petr Nikitin <nikitin.p.94@gmail.com>
2021-05-03 23:23:14 +0300

    fix mock

--- src/__mocks__/MockConsumer.tsx
+++ src/__mocks__/MockConsumer.tsx
@@ -6,7 +6,11 @@ const MockConsumer = () => {
   return (
     <div>
       <p>Auth: {auth.isLoggedIn.toString()}</p>
-      <button onClick={() => auth.login({ email: "test", password: "test" }, false)}>
+      <button
+        onClick={() =>
+          auth.login({ email: "test", password: "test", project: "" }, false)
+        }
+      >
         Login
       </button>
       <button onClick={() => auth.checkAuth()}>Check</button>
