d45fd2d88293ab69a26db277155d1d87d2b8e52a
Petr <nikitin.p.94@gmail.com>
2021-05-19 21:23:48 +0300

    add unAuth btn

--- src/client/script/pages/Main.tsx
+++ src/client/script/pages/Main.tsx
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
 
-import { Accordion, Icon, Label } from "semantic-ui-react";
+import { Accordion, Icon, Label, Button } from "semantic-ui-react";
 
 import LoginByAdmin from "client/script/components/LoginByAdmin";
 import ScenarioForm from "client/script/components/ScenarioForm";
 
-import { StartScenarioBody, ScenarioNames } from "src/declarations";
-import { handleProjectNameInput } from "client/script/helpers/inputChanges";
-import startScenario from "client/script/api/scenarioRequests";
 import useAuth from "client/script/hooks/useAuth";
-import scenarios from "src/data";
 
 import "client/styles/block/form/form.css";
 
 const Main = () => {
   const auth = useAuth();
 
-  const [isAuthShown, setIsAuthShown] = useState(!auth.isLoggedIn);
+  const [isAuthShown, setIsAuthShown] = useState(false);
+
+  useEffect(() => {
+    setIsAuthShown(!auth.isLoggedIn);
+  }, [auth.isLoggedIn]);
 
   return (
     <>
@@ -29,9 +28,21 @@ const Main = () => {
         >
           <Icon name="dropdown" />
           {auth.loginForProject ? (
-            <Label color="green">
-              Авторизован в проекте: {auth.loginForProject}.mindbox.ru
-            </Label>
+            <>
+              <Label color="green">
+                Авторизован в проекте: {auth.loginForProject}.mindbox.ru
+              </Label>{" "}
+              <Button
+                basic
+                size="mini"
+                onClick={() => {
+                  document.cookie = "token=";
+                  auth.checkAuth();
+                }}
+              >
+                Сменить проект
+              </Button>
+            </>
           ) : (
             <Label color="orange">Нужно авторизоваться</Label>
           )}
