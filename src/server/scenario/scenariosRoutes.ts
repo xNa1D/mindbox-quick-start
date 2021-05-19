4255c92ac8badd987d2394d6bada4b4a3f3c41f7
Petr <nikitin.p.94@gmail.com>
2021-05-19 20:54:45 +0300

    moved the function to a separate file

--- src/server/scenario/scenariosRoutes.ts
+++ src/server/scenario/scenariosRoutes.ts
@@ -1,16 +1,9 @@
 import { Request, Response, NextFunction, Router } from "express";
 
-import startScenario from "./startScenario";
-import checkToken  from "../user/checkTocken";
-import sendMessage from "./sendMessage";
-import { operations } from "../../config";
+import checkToken from "../user/checkTocken";
+import startScenarioAndSendResult from "./startScenarioAndSendResult";
 
-import {
-  StartScenarioBody,
-  SuccessMessageParameters,
-  ErrorMessageParameters,
-  JwtUser
-} from "src/declarations";
+import { StartScenarioBody, JwtUser } from "src/declarations";
 
 const scenariosRoutes = Router();
 scenariosRoutes.post(
@@ -20,63 +13,26 @@ scenariosRoutes.post(
     res: Response,
     next: NextFunction
   ) => {
-    let user:JwtUser; 
+    let user: JwtUser;
     try {
       user = checkToken(req.cookies.token);
+      // jwt is OK. will start scenario async
       res.sendStatus(200);
 
       const projectName = user.project || "";
       const scenario = req.body.scenario;
       const campaign = req.body.campaign;
 
-      try {
-        const scenarioResult = await startScenario(
-          scenario.api,
-          projectName,
-          campaign
-        );
-
-        if (scenarioResult.status === "SUCCESS") {
-          sendMessage<SuccessMessageParameters>({
-           email: user.email,
-            mailingParams: {
-              documentationLink: scenario.docs,
-              projectName,
-              steps: scenarioResult.steps,
-              task: scenario.name,
-            },
-            operation: operations.messages.success,
-          });
-        } else {
-          const errorMessagePayload: ErrorMessageParameters = {
-            projectName,
-            videoLink: scenarioResult.error.videoLink,
-            errorMessage: scenarioResult.error.errorMessage,
-            steps: scenarioResult.steps,
-            task: scenario.name,
-          };
-
-          sendMessage<ErrorMessageParameters>({
-            email: user.email,
-            mailingParams: errorMessagePayload,
-            operation: operations.messages.error,
-          });
-          sendMessage<ErrorMessageParameters>({
-            email: "nikitin@mindbox.ru",
-            mailingParams: errorMessagePayload,
-            operation: operations.messages.error,
-          });
-          sendMessage<ErrorMessageParameters>({
-            email: "moskalev@mindbox.ru",
-            mailingParams: errorMessagePayload,
-            operation: operations.messages.error,
-          });
-        }
-      } catch (error) {
-        throw error;
-      }
+      await startScenarioAndSendResult({
+        user,
+        projectName,
+        scenario,
+        campaign,
+      });
     } catch (error) {
-      res.status(403).send(error);
+      res
+        .status(403)
+        .send("Ошибка запуска. Перезагрузите страницу и попробуйте еще раз");
       next();
     }
   }
