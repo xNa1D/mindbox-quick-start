756ee12f6d16926f9b226739276a46250d60f501
Petr Nikitin <nikitin.p.94@gmail.com>
2021-09-04 14:44:47 +0300

    move info to own component

--- src/client/script/components/ScenarioForm.tsx
+++ src/client/script/components/ScenarioForm.tsx
@@ -120,37 +120,8 @@ const Scenario = () => {
           </form>
         </div>
         <div className="six wide column">
-          <div className="ui message">
-            <div className="header">Что дальше?</div>
-            <ul className="list">
-              <li>
-                Сейчас мы запустили сценарий автозаведения. На проекте
-                создадутся точки интеграции, операции, ШД и прочее.{" "}
-              </li>
-              <li>Минут через 10-15 должно прийти письмо с результатом</li>
-              <li>
-                Пока оно заводится, сделай копию гугльдока и замени в нем на
-                название проекта
-              </li>
-              <li>
-                После получения письма обязательно проверь все операции по ТЗ.
-                Если чего-то нет, то напиши про это в слаке{" "}
-                <a href="https://mindbox.slack.com/archives/C01G12FQQ0Z">
-                  #mindbox-quick-start
-                </a>
-              </li>
-            </ul>
-          </div>
-          <a
-            className="ui button green fluid"
-            href={scenario.scenario.docs}
-            id="lintToTZ"
-            target="_blank"
-          >
-            <i className="file alternate outline icon"></i>Заготовка под ТЗ{" "}
-          </a>
+          <ScenarioInfo documentationLink={scenario.scenario.docs} />
         </div>
-      </div>
     </>
   );
 };
