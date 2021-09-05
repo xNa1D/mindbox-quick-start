fae30b5b100fd4cf5f60f520afc4aeef614dd629
Petr Nikitin <nikitin.p.94@gmail.com>
2021-09-05 21:02:44 +0300

    add dynamic description

--- src/client/scenario/ScenarioInfo.tsx
+++ src/client/scenario/ScenarioInfo.tsx
@@ -2,47 +2,56 @@ import React, { useEffect, useState } from "react";
 import { Button, Header, List, Message, Segment } from "semantic-ui-react";
 import ReactMarkdown from "react-markdown";
 
+import getScenarioDescription from "./getScenarioDescription";
 
 type ScenarioInfo = {
   documentationLink: string;
+  scenarioType: string;
 };
 
-const ScenarioInfo = ({ documentationLink }: ScenarioInfo) => {
+const ScenarioInfo = ({ documentationLink, scenarioType }: ScenarioInfo) => {
+  const [scenarioDescription, setScenarioDescription] = useState("");
+  const [isDescriptionLoading, setIsDescriptionLoading] = useState(false);
+  const [gettingDescriptionError, setGettingDescriptionError] = useState("");
+
+  useEffect(() => {
+    setIsDescriptionLoading(true);
+    getScenarioDescription(scenarioType)
+      .then((description) => setScenarioDescription(description))
+      .catch((error) => setGettingDescriptionError(error.toString()))
+      .finally(() => setIsDescriptionLoading(false));
+  }, [scenarioType]);
+
   return (
     <>
-      <div className="ui message">
-        <div className="header">Что дальше?</div>
-        <ul className="list">
-          <li>
-            Сейчас мы запустили сценарий автозаведения. На проекте создадутся
-            точки интеграции, операции, ШД и прочее.{" "}
-          </li>
-          <li>Минут через 10-15 должно прийти письмо с результатом</li>
-          <li>
-            Пока оно заводится, сделай копию гугльдока и замени в нем на
-            название проекта
-          </li>
-          <li>
-            После получения письма обязательно проверь все операции по ТЗ. Если
-            чего-то нет, то напиши про это в слаке{" "}
-            <a href="https://mindbox.slack.com/archives/C01G12FQQ0Z">
-              #mindbox-quick-start
-            </a>
-          </li>
-        </ul>
+      <div style={{ paddingBottom: "1rem" }}>
+        <Header as="h3">Как это работает?</Header>
+        <List ordered>
+          <List.Item>Авыторизуетесь в проекте</List.Item>
+          <List.Item>Выбираете сценарий, кампанию и запускаете</List.Item>
+          <List.Item>
+            Бот войдет в нужный проект и заведет все, что нужно для стандартного
+            ТЗ
+          </List.Item>
+          <List.Item>
+            Когда бот закончит, на почту прийдет письмо со статусом по каждой
+            операции
+          </List.Item>
+        </List>
       </div>
+      <Segment loading={isDescriptionLoading}>
+        <ReactMarkdown>{scenarioDescription}</ReactMarkdown>
+        {gettingDescriptionError && (
+          <Message error visible content={gettingDescriptionError} />
+        )}
+      </Segment>
       {documentationLink && (
-        <a
-          className="ui button green fluid"
-          href={documentationLink}
-          id="lintToTZ"
-          target="_blank"
-        >
+        <Button positive href={documentationLink} id="lintToTZ" target="_blank">
           <i className="file alternate outline icon"></i>Заготовка под ТЗ{" "}
-        </a>
+        </Button>
       )}
     </>
   );
-}
+};
 
 export default ScenarioInfo;
