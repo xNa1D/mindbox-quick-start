d5b0bc80b74df7d3ee173a422b33ff9dfd78eb5d
Petr Nikitin <nikitin.p.94@gmail.com>
2021-09-05 18:11:46 +0300

    add footer

--- src/client/Main.tsx
+++ src/client/Main.tsx
@@ -4,23 +4,27 @@ import ScenarioForm from "client/scenario/ScenarioForm";
 import AuthComponent from "./auth/AuthComponent";
 
 import "client/styles/block/form/form.css";
-import { Container, Grid, Menu, Segment } from "semantic-ui-react";
+import { Container, Grid, Icon, Menu, Segment } from "semantic-ui-react";
 import Logo from "./common/Logo";
 
 const Main = () => {
   return (
-    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
-      <section>
+    <div
+      style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+    >
+      <section style={{ flexGrow: 1 }}>
         <AuthComponent />
       </section>
-      <Segment vertical style={{ padding: "5em 0em" }}>
+      <Segment vertical style={{ padding: "5em 0em", flexGrow: 3 }}>
         <main className="main">
           <ScenarioForm />
         </main>
       </Segment>
-        <Segment inverted vertical style={{ padding: "5em 0em"}}>
-          <Container></Container>
-        </Segment>
+      <Segment inverted vertical style={{ padding: "3em 0em" }}>
+        <Container>
+          <Icon name="code" /> with <Icon name="heart" color="red" />
+        </Container>
+      </Segment>
     </div>
   );
 };
