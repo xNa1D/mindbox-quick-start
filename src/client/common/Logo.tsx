62096fbe59268fc98bd29121dba985da2480fc06
Petr Nikitin <nikitin.p.94@gmail.com>
2021-09-09 22:56:11 +0300

    fix logo size

--- src/client/common/Logo.tsx
+++ src/client/common/Logo.tsx
@@ -1,20 +1,21 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties } from "react";
 import logo from "client/images/logo.png";
 
 const Logo = () => {
-
   const style: CSSProperties = {
-    width: "200px"
-  } 
+    width: "200px",
+  };
 
   return (
+    <div style={{ maxWidth: "200px" }}>
       <img
         className="auth__logo"
         src={logo}
         alt="Логотип Mindbox Quick Start"
         style={style}
       />
+    </div>
   );
-}
+};
 
-export default Logo
+export default Logo;
