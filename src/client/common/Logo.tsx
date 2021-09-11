4eda0e210a038cf9b6d35df8cdfe65f15e082ce4
Petr Nikitin <nikitin.p.94@gmail.com>
2021-09-11 17:22:32 +0300

    refacto rstyles

--- src/client/common/Logo.tsx
+++ src/client/common/Logo.tsx
@@ -1,18 +1,14 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 import logo from "client/images/logo.png";
 
 const Logo = () => {
-  const style: CSSProperties = {
-    width: "200px",
-  };
-
   return (
     <div style={{ maxWidth: "200px" }}>
       <img
         className="auth__logo"
         src={logo}
         alt="Логотип Mindbox Quick Start"
-        style={style}
+        style={{ width: "200px" }}
       />
     </div>
   );
