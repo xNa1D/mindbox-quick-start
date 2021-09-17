0442d3768c6c9a0dd445a23b96722c09a40d2e92
Petr <nikitin.p.94@gmail.com>
2021-09-17 12:54:59 +0300

    fix typos

--- src/client/scenario/ScenarioInstruction.tsx
+++ src/client/scenario/ScenarioInstruction.tsx
@@ -6,14 +6,14 @@ const ScenarioInstruction = () => {
     <Segment color="yellow" raised>
       <Header as="h3">Как это работает:</Header>
       <List ordered>
-        <List.Item>Авыторизуетесь в проекте</List.Item>
+        <List.Item>Авторизуетесь в проекте</List.Item>
         <List.Item>Выбираете сценарий, кампанию и запускаете</List.Item>
         <List.Item>
           Бот войдет в нужный проект и заведет все, что нужно для стандартного
           ТЗ
         </List.Item>
         <List.Item>
-          Когда бот закончит, на почту прийдет письмо со статусом по каждой
+          Когда бот закончит, на почту придет письмо со статусом по каждой
           операции
         </List.Item>
         <List.Item>
