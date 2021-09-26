42f2892d91aeb9ea3dfbb098fa4f267e34604b2d
Petr Nikitin <nikitin.p.94@gmail.com>
2021-09-26 23:03:26 +0300

    add test for yml module

---
+++ src/__tests__/server/yml/createYmlData.test.ts
@@ -0,0 +1,60 @@
+import createYmlData, {
+  AuthParams,
+  Link,
+  Settings,
+} from "src/server/yml/createYmlData";
+import { YmlImportSetting } from "src/server/yml/sendYmlToMindbox";
+
+const mockLinks: Link = {
+  url: "linkToFeed",
+  name: "awesomeName",
+  externalId: "areaId",
+};
+
+const mockSettings: Settings = {
+  brand: "myBrand",
+  externalSystem: "mySystem",
+  launchPeriod: 2,
+};
+
+const mockAuth: AuthParams = {
+  password: "myPassword",
+  username: "iAm",
+};
+
+const resultYml: YmlImportSetting = {
+  area: {
+    externalId: mockLinks.externalId,
+  },
+  name: mockLinks.name,
+  url: mockLinks.url,
+  brandSystemName: mockSettings.brand,
+  externalSystemSystemName: mockSettings.externalSystem,
+  launchPeriod: mockSettings.launchPeriod.toString(),
+  password: null,
+  username: null,
+};
+const resultYmlWithAuth: YmlImportSetting = {
+  area: {
+    externalId: mockLinks.externalId,
+  },
+  name: mockLinks.name,
+  url: mockLinks.url,
+  brandSystemName: mockSettings.brand,
+  externalSystemSystemName: mockSettings.externalSystem,
+  launchPeriod: mockSettings.launchPeriod.toString(),
+  password: mockAuth.password,
+  username: mockAuth.username,
+};
+
+test("should return settings without auth", () => {
+  const ymlData = createYmlData([mockLinks], mockSettings);
+
+  expect(ymlData).toStrictEqual([resultYml]);
+});
+
+test("should return settings with auth", () => {
+  const ymlData = createYmlData([mockLinks], mockSettings, mockAuth);
+  
+  expect(ymlData).toStrictEqual([resultYmlWithAuth]);
+});
