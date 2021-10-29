97b2214c75720d04e86c23096d74d4214a89b989
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-29 20:27:29 +0300

    change settings

--- src/server/db/init.ts
+++ src/server/db/init.ts
@@ -3,12 +3,14 @@ import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: path.resolve(__dirname, "../../data/database.sqlite"),
+  storage: path.resolve(__dirname, "../../../data/database.sqlite"),
 });
 
 export const initDb = async () => {
   try {
     await sequelize.authenticate();
+    sequelize.sync({ alter: true });
+
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
