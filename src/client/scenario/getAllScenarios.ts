70cbd896896e9bd4d78074e2283b00ae022a3639
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-31 21:06:52 +0300

    fix api url

--- src/client/scenario/getAllScenarios.ts
+++ src/client/scenario/getAllScenarios.ts
@@ -2,6 +2,6 @@ import { Scenario } from "src/declarations";
 import axios from "axios";
 
 export const getAllScenarios = async () => {
-  const result = await axios.get<Scenario[]>("/scenarios");
+  const result = await axios.get<Scenario[]>("/api/scenario");
   return result.data;
-}
+};
