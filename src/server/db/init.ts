fae637d15fab6e8a9e4ad7434393af4c11b71dc9
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-31 21:05:21 +0300

    off loging

--- src/server/db/init.ts
+++ src/server/db/init.ts
@@ -4,6 +4,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: path.resolve(__dirname, "../../../data/database.sqlite"),
+  logging: false,
 });
 
 export const initDb = async () => {
