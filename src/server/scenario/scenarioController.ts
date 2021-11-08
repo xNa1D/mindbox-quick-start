f368de42fe202cc9ed5c9539044a65b6f0093c86
Petr Nikitin <nikitin.p.94@gmail.com>
2021-11-08 19:42:38 +0300

    refactor controller to working with DB

--- src/server/scenario/scenarioController.ts
+++ src/server/scenario/scenarioController.ts
@@ -1,71 +1,18 @@
-import fs from "fs";
-import path from "path";
 import { Scenario } from "src/declarations";
+import { addNew, getAll, update } from "../db/model";
 
-const fileWithScenarios = "../../../data/scenarios.json";
-
-const convertToScenarioJSON = (rawScenario: Buffer): Scenario[] =>
-  JSON.parse(rawScenario.toString());
-
-const getScenariosFromFile = () => {
-  return new Promise<Scenario[]>((resolve, reject) => {
-    fs.readFile(
-      path.resolve(__dirname, fileWithScenarios),
-      (err, scenarios) => {
-        if (err) {
-          reject(err);
-        }
-
-        resolve(convertToScenarioJSON(scenarios));
-      }
-    );
-  });
-};
-
-const saveScenarios = (newData: Scenario[]) => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(
-      path.resolve(__dirname, fileWithScenarios),
-      JSON.stringify(newData),
-      (err) => {
-        if (err) {
-          reject(err);
-        }
-      }
-    );
-  });
-};
 
 export const getAllScenarios = async () => {
-  return await getScenariosFromFile();
+  return await getAll();
 };
 
 export const addNewScenario = async (scenario: Scenario) => {
-  const scenarioArray: Scenario[] = await getScenariosFromFile();
-
-
-  
-  
-  if ((scenarioArray.find((item) => item.type === scenario.type))) {
-    console.log(scenarioArray.find((item) => item.type === scenario.type));
-    
-    throw new Error("Такой сценарий уже есть");
-  }
-
-  scenarioArray.push(scenario);
-  saveScenarios(scenarioArray);
+  await addNew(scenario);
 
   return true;
 };
 
 export const updateScenario = async (scenario: Scenario) => {
-  const scenarioArray: Scenario[] = await getScenariosFromFile();
-
-  const updatedScenarioArray = scenarioArray.map((item) => {
-    return item.type === scenario.type ? scenario : item;
-  });
-
-  saveScenarios(updatedScenarioArray);
-
+  await update(scenario);
   return true;
 };
