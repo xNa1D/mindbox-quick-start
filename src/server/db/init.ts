c46481a7d6a3ef2bf96672898c98b50523f942c6
Petr <nikitin.p.94@gmail.com>
2021-11-22 11:43:44 +0300

    change sequlize sync to alter

--- src/server/db/init.ts
+++ src/server/db/init.ts
@@ -10,7 +10,7 @@ export const sequelize = new Sequelize({
 export const initDb = async () => {
   try {
     await sequelize.authenticate();
-    sequelize.sync({ force: true });
+    sequelize.sync({ alter: true });
 
     console.log("Connection has been established successfully.");
   } catch (error) {
