28098af81bc39fc2ad1f2f56a5e50db65028c43a
Petr <nikitin.p.94@gmail.com>
2021-11-29 20:59:02 +0300

    Add route for CF start

---
+++ src/server/custom-fields/routes/addCfRoute.ts
@@ -0,0 +1,29 @@
+import { Request, Response, Router } from "express";
+
+import { authMiddleware } from "../../auth";
+import { runAndNotify } from "../runAndNotify";
+
+import { StartScenarioBody } from "src/declarations";
+
+import { config } from "../../../config";
+import { CustomFieldObject } from "..";
+
+const addingCfRoutes = Router();
+
+addingCfRoutes.post(
+  "/start",
+  authMiddleware,
+  async (req: Request<{}, {}, { cfs: CustomFieldObject[] }>, res: Response) => {
+    try {
+      runAndNotify(req.body.cfs, res.locals.user);
+
+      res.sendStatus(200);
+    } catch (error) {
+      res
+        .status(403)
+        .send("Ошибка запуска. Перезагрузите страницу и попробуйте еще раз");
+    }
+  }
+);
+
+export default addingCfRoutes;
