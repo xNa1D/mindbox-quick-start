0039cd0ec2c9c7f7e2f20c5a3a8ca73ba3798c36
Petr <nikitin.p.94@gmail.com>
2021-12-07 20:01:31 +0300

    lint code

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,57 +1,54 @@
-const path = require("path");
-const { merge } = require("webpack-merge");
+import { resolve as _resolve, join } from "path";
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ProvidePlugin } = require("webpack");
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { ProvidePlugin } from "webpack";
 
-module.exports = {
-  entry: path.resolve(__dirname, "../src/client", "index.tsx"),
-  target: "web",
-  output: {
-    path: path.resolve(__dirname, "../build/client"),
-    filename: "[name].[contenthash].js",
-    chunkFilename: "chunk-[name].[contenthash].js",
+export const entry = _resolve(__dirname, "../src/client", "index.tsx");
+export const target = "web";
+export const output = {
+  path: _resolve(__dirname, "../build/client"),
+  filename: "[name].[contenthash].js",
+  chunkFilename: "chunk-[name].[contenthash].js",
+};
+export const resolve = {
+  extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+  // fallback: { "assert": require.resolve("assert/") },
+  alias: {
+    src: _resolve(__dirname, "../src/"),
+    client: _resolve(__dirname, "../src/client/"),
+    server: _resolve(__dirname, "../src/server/"),
   },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
-    // fallback: { "assert": require.resolve("assert/") },
-    alias: {
-      src: path.resolve(__dirname, "../src/"),
-      client: path.resolve(__dirname, "../src/client/"),
-      server: path.resolve(__dirname, "../src/server/"),
+  fallback: {
+    assert: require.resolve("assert/"),
+  },
+};
+export const module = {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: "ts-loader",
+      exclude: [_resolve(__dirname, "./src/server/**/*"), /node_modules/],
     },
-    fallback: {
-      assert: require.resolve("assert/"),
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: [path.resolve(__dirname, "./src/server/**/*"), /node_modules/],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg|png|jpe?g|gif)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.join(__dirname, "../src/client", "index.html"),
-    }),
-    new ProvidePlugin({
-      process: "process/browser",
-    }),
+    {
+      test: /\.(woff(2)?|ttf|eot|svg|png|jpe?g|gif)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    },
   ],
 };
+export const plugins = [
+  new HtmlWebpackPlugin({
+    inject: true,
+    template: join(__dirname, "../src/client", "index.html"),
+  }),
+  new ProvidePlugin({
+    process: "process/browser",
+  }),
+];
