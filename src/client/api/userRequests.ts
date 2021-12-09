c1a284f182c81e44dc1e742edaa180991ac0d5a9
Petr <nikitin.p.94@gmail.com>
2021-12-09 18:49:46 +0300

    separated to constant

--- src/client/api/userRequests.ts
+++ src/client/api/userRequests.ts
@@ -6,27 +6,23 @@ import {
   AuthByAdminPanelRequestBody,
 } from "src/declarations";
 
+const options = {
+  headers: { "content-type": "application/json" },
+};
+
 export const loginUser = (user: AuthRequestBody, isLoginByAdmin: boolean) => {
   const authUrl = isLoginByAdmin ? "authByAdminPanel" : "auth";
 
-  return axios.post(`/api/user/${authUrl}`, user, {
-    headers: { "content-type": "application/json" },
-  });
+  return axios.post(`/api/user/${authUrl}`, user, options);
 };
 
 export const loginUserByAdminPanel = (user: AuthByAdminPanelRequestBody) => {
-  return axios.post("/api/user/authByAdminPanel", user, {
-    headers: { "content-type": "application/json" },
-  });
+  return axios.post("/api/user/authByAdminPanel", user, options);
 };
 
 export const checkToken = () => {
-  return axios.get("/api/user/checkToken", {
-    headers: { "content-type": "application/json" },
-  });
+  return axios.get("/api/user/checkToken", options);
 };
 export const createUser = (user: RegistrationRequest) => {
-  return axios.post("/api/user/reg", user, {
-    headers: { "content-type": "application/json" },
-  });
+  return axios.post("/api/user/reg", user, options);
 };
