e6f87030f93d1e3214859d1ccba872162c2a1a7d
Petr Nikitin <nikitin.p.94@gmail.com>
2021-12-11 22:47:46 +0300

    Fix form of data

--- src/client/shared/api/startCF.ts
+++ src/client/shared/api/startCF.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { CustomFieldObject } from "src/server/custom-fields";
 
-
 export const startCf = async (data: CustomFieldObject[]) =>
-  await axios.post("/api/cf/start", data, {
-    headers: { "content-type": "application/json" },
-  });
-
+  await axios.post(
+    "/api/cf/start",
+    { cfs: data },
+    {
+      headers: { "content-type": "application/json" },
+    }
+  );
