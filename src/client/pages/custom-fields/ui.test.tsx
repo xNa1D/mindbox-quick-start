7586219b231bbce98f03564543f2a838d6b4c0de
Petr Nikitin <nikitin.p.94@gmail.com>
2021-12-11 21:54:59 +0300

    Add tests of rendering after parsing csv

--- src/client/pages/custom-fields/ui.test.tsx
+++ src/client/pages/custom-fields/ui.test.tsx
@@ -5,11 +5,9 @@ import "@testing-library/jest-dom";
 import { CustomFields } from ".";
 
 import { startCf } from "client/shared/api/startCF";
-import { parseCsv } from "client/processes/csv-to-json";
 
 jest.mock("axios");
 jest.mock("client/shared/api/startCF");
-jest.mock("client/processes/csv-to-json");
 
 const FileInputLabel = "Файл с доп полями";
 const btnText = "Запустить заведение доп полей";
@@ -23,7 +21,7 @@ const csvFile = new File([csvString], "table.csv", {
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-const setUpYmlForm = () => {
+const setUp = () => {
   let component!: RenderResult;
 
   act(() => {
@@ -47,25 +45,27 @@ const inputFile = (input: HTMLElement, value: string | number | File) => {
 
 describe("CustomFields", () => {
   test("should render file input", async () => {
-    const { component } = setUpYmlForm();
+    const { component } = setUp();
 
     expect(component.queryByLabelText(FileInputLabel)).toBeInTheDocument();
   });
 
-  test("should call parseCsv on file update", async () => {
-    const { component } = setUpYmlForm();
+  test("When csv is filled, should render data from file", async () => {
+    const { component } = setUp();
 
     const fileInput = component.getByLabelText(FileInputLabel);
 
     inputFile(fileInput, csvFile);
-    // TODO: Fix test, should expect that after passing csv calls parser
-    expect((parseCsv as jest.Mock).mock.calls[0][0].name).toBe("table.csv");
+
+    await delay(500);
+
+    expect(component.getByText("SystemName")).toBeInTheDocument();
   });
 });
 
 describe("sendData", () => {
-  test("When all fields filledin, should pass their data", async () => {
-    const { component } = setUpYmlForm();
+  test("When all fields filled, should pass their data", async () => {
+    const { component } = setUp();
 
     const fileInput = component.getByLabelText(FileInputLabel);
     const btn = component.getByText(btnText);
