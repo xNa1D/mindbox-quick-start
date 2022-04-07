fb94fb197cdaf07004fea14ea958f06dc3d33c39
Nikitin Petr <nikitin.p.94@gmail.com>
2022-04-07 17:34:36 +0300

    fix test

--- src/client/entities/cf-preview/ui.test.tsx
+++ src/client/entities/cf-preview/ui.test.tsx
@@ -1,8 +1,8 @@
 import "@testing-library/jest-dom";
 import { act, render, screen } from "@testing-library/react";
 import React from "react";
+import { columns } from "src/client/pages/custom-fields";
 import { CustomFieldObject } from "src/server/custom-fields";
-import { column } from "../csv-instruction";
 import { CfPreview } from "./ui";
 
 const customRender = async (ui: React.ReactElement) => {
@@ -10,28 +10,6 @@ const customRender = async (ui: React.ReactElement) => {
     render(ui);
   });
 };
-
-const columns: column[] = [
-  { header: "CustomFieldEntity", description: "Сущность", isRequired: true },
-  { header: "CustomFieldName", description: "Название", isRequired: true },
-  {
-    header: "CustomFieldSystemName",
-    description: "Системное имя",
-    isRequired: true,
-  },
-  {
-    header: "CustomFieldValueTypes",
-    description: "Тип данных",
-    isRequired: true,
-  },
-  {
-    header: "isClearable",
-    description: "Очищать, если не передано",
-    isRequired: true,
-  },
-  { header: "isMultiple", description: "Множественное", isRequired: true },
-  { header: "isPublic", description: "Публичное", isRequired: true },
-];
 
 const data: CustomFieldObject[] = [
   {
@@ -49,24 +27,24 @@ describe("CfPreview", () => {
   test("When render table, should render headers", async () => {
     await customRender(<CfPreview columns={columns} data={data} />);
 
-    expect(screen.getByText(columns[0].description as string)).toBeInTheDocument();
+    expect(screen.getByText(columns[0].header as string)).toBeInTheDocument();
     expect(
-      screen.getByText(columns[1].description as string)
+      screen.getByText(columns[1].header as string)
     ).toBeInTheDocument();
     expect(
-      screen.getByText(columns[2].description as string)
+      screen.getByText(columns[2].header as string)
     ).toBeInTheDocument();
     expect(
-      screen.getByText(columns[3].description as string)
+      screen.getByText(columns[3].header as string)
     ).toBeInTheDocument();
     expect(
-      screen.getByText(columns[4].description as string)
+      screen.getByText(columns[4].header as string)
     ).toBeInTheDocument();
     expect(
-      screen.getByText(columns[5].description as string)
+      screen.getByText(columns[5].header as string)
     ).toBeInTheDocument();
     expect(
-      screen.getByText(columns[6].description as string)
+      screen.getByText(columns[6].header as string)
     ).toBeInTheDocument();
   });
 
